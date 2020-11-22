5e2d02802377b40f74d486f6cd2da2bbff65ceb8
somuberry <saumyaoberoi20@gmail.com>
2020-11-22 14:45:06 +0530

    Update bootstrap-4.4.1.js

--- js/bootstrap-4.4.1.js
+++ js/bootstrap-4.4.1.js
@@ -37,6 +37,13 @@ img[x].onclick = function(){
   captionText.innerHTML = this.alt;
 }
 }
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("closebox")[0];
+										
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
 
 
 // Get the <span> element that closes the modal
